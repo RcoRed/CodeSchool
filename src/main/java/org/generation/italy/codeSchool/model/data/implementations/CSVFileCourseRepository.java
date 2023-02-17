be1991af72f47c03146ebdb247173a809c000514
GabrielAgustinIppolito <gabrielpplt@gmail.com>
2023-02-17 13:04:39 +0100

    Aggiunge metodo deleteById a CSVFileCourseRepository.java e il rispettivo test

--- src/main/java/org/generation/italy/codeSchool/model/data/implementations/CSVFileCourseRepository.java
+++ src/main/java/org/generation/italy/codeSchool/model/data/implementations/CSVFileCourseRepository.java
@@ -5,81 +5,102 @@ import org.generation.italy.codeSchool.model.data.abstructions.CourseRepository;
 import org.generation.italy.codeSchool.model.data.exceptions.DataException;
 import org.generation.italy.codeSchool.model.data.exceptions.EntityNotFoundException;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 
 public class CSVFileCourseRepository implements CourseRepository {
-    private String fileName;
-    private static long nextId;
-    public static final String DEFAULT_FILE_NAME = "Corsi.csv";
+   private String fileName;
+   private static long nextId;
+   public static final String DEFAULT_FILE_NAME = "Corsi.csv";
 
-    public CSVFileCourseRepository() {
-        this.fileName = DEFAULT_FILE_NAME;
-    }
-    public CSVFileCourseRepository(String fileName) {
-        this.fileName = fileName;
-    }
+   public CSVFileCourseRepository() {
+      this.fileName = DEFAULT_FILE_NAME;
+   }
+   public CSVFileCourseRepository(String fileName) {
+      this.fileName = fileName;
+   }
 
-    @Override
-    public Optional<Course> findById(long id) throws DataException{             //!!RICORDATI!! se un metodo può dare un errore allora DEVI mettere il THROWS e l'exception che "lancerà"
-        try{
-            List<String> lines = Files.readAllLines(Paths.get(fileName));       //apro il file
-            for (String s:lines){                                               //ciclo per ogni riga letta
-                String[] trimmed = s.split(",");                          //uso un metodo della classe String che creerà una nuova stringa per ogni , che incontrerà, ogni stringa verrà salvata in un array
-                long courseId = Long.parseLong(trimmed[0]);
-                if (courseId == id){
-                    Course found = new Course(courseId,trimmed[1],trimmed[2]    //creo l'oggeto passando le stringhe letta dal file
-                            ,trimmed[3],Double.parseDouble(trimmed[4]));
-                    return Optional.of(found);
-                }
+   @Override
+   public Optional<Course> findById(long id) throws DataException{             //!!RICORDATI!! se un metodo può dare un errore allora DEVI mettere il THROWS e l'exception che "lancerà"
+      try{
+         List<String> lines = Files.readAllLines(Paths.get(fileName));       //apro il file
+         for (String s:lines){                                               //ciclo per ogni riga letta
+            String[] trimmed = s.split(",");                          //uso un metodo della classe String che creerà una nuova stringa per ogni , che incontrerà, ogni stringa verrà salvata in un array
+            long courseId = Long.parseLong(trimmed[0]);
+            if (courseId == id){
+               Course found = new Course(courseId,trimmed[1],trimmed[2]    //creo l'oggeto passando le stringhe letta dal file
+                     ,trimmed[3],Double.parseDouble(trimmed[4]));
+               return Optional.of(found);
             }
-            return Optional.empty();
-        }catch (IOException e){                                                  //"raccogliamo" IOException ma "lanciamo" un DataException di nostra creazione (si, si può fare) così da poter ancora utilizzare la nostra interfaccia nel miglior modo possibile
-            throw new DataException("Errore nella lettura del file",e);          //lo lanciamo qui
-        }
-    }
+         }
+         return Optional.empty();
+      }catch (IOException e){                                                  //"raccogliamo" IOException ma "lanciamo" un DataException di nostra creazione (si, si può fare) così da poter ancora utilizzare la nostra interfaccia nel miglior modo possibile
+         throw new DataException("Errore nella lettura del file",e);          //lo lanciamo qui
+      }
+   }
 
-    @Override
-    public List<Course> findByTitleContains(String part) {
-        return null;
-    }
+   @Override
+   public List<Course> findByTitleContains(String part) {
+      return null;
+   }
 
-    @Override
-    public Course create(Course course) throws DataException{
+   @Override
+   public Course create(Course course) throws DataException{
         /*
             FileOutputStream serve per scrivere nel file                !!(magari da richiedere)!!
             quel (append)true serve ad aggiungere una nuova riga alle riche esistenti, se non ci fosse sovrascriverebbe tutte le righe presenti nel file
             PintWriter sarà colui che effettivamente scriverà sul file
          */
-        try (FileOutputStream output = new FileOutputStream(fileName,true);
-                PrintWriter pw = new PrintWriter(output)){
-            course.setId(++nextId);
-            pw.println(CourseToCSV(course));                //è qui che scrivo sul file (si con una println) richiamando un metodo creato da noi(sta verso la fine)
-            return course;                                  //ovviamente nelle parentesi gli passo la stringa che voglio sivere sul file
-        }catch (IOException e){
-            throw new DataException("Errore nel salvataggio su file",e);
-        }
-    }
+      try (FileOutputStream output = new FileOutputStream(fileName,true);
+           PrintWriter pw = new PrintWriter(output)){
+         course.setId(++nextId);
+         pw.println(CourseToCSV(course));                //è qui che scrivo sul file (si con una println) richiamando un metodo creato da noi(sta verso la fine)
+         return course;                                  //ovviamente nelle parentesi gli passo la stringa che voglio sivere sul file
+      }catch (IOException e){
+         throw new DataException("Errore nel salvataggio su file",e);
+      }
+   }
 
-    @Override
-    public void update(Course course) throws EntityNotFoundException {
+   @Override
+   public void update(Course course) throws EntityNotFoundException {
 
-    }
+   }
 
-    @Override
-    public void deleteById(long id) throws EntityNotFoundException {
-        System.out.println("Aggiunto questo solo per spiegare GitWorkFlow");
-        //lollo
-    }
+   @Override
+   public void deleteById(long id) throws EntityNotFoundException, DataException {
+      try{
+         List<String> lines = Files.readAllLines(Paths.get(fileName));
+         for (Iterator<String> it = lines.iterator(); it.hasNext();) { //iterator è uno dei 23 design pattern, voglio trovare un modo uniforme per iterare  su tante diverse forme di struttre dati
+            String line = it.next();    //questo è quello che fa veramente un forEach
+            String[] tokens = line.split(",");
+            long courseId = Long.parseLong(tokens[0]);
+            if (courseId == id){
+               it.remove();
+               try(PrintWriter pW = new PrintWriter(new FileOutputStream(fileName))) {     //serve solo a fare gestione delle risorse
+                  for (String st : lines) {
+                     pW.println(st);
+                  }
+               }
+               return;
+            }
+         }
+         throw new EntityNotFoundException("Non esiste un corso con id: " + id);
+      }catch(IOException e){
+         throw new DataException("Errore nel cancellamento di una linea da file csv. ", e);
+      }
 
-    public String CourseToCSV(Course c){                //trasforma i dati presenti dell'oggetto in una stringa(che poi scriveremo sul file)
-        return String.format(Locale.US,"%d,%s,%s,%s,%.2f",c.getId(),c.getTitle()
-                ,c.getDescription(),c.getProgram(),c.getDuration());
-    }
+   }
+
+   public String CourseToCSV(Course c){                //trasforma i dati presenti dell'oggetto in una stringa(che poi scriveremo sul file)
+      return String.format(Locale.US,"%d,%s,%s,%s,%.2f",c.getId(),c.getTitle()
+            ,c.getDescription(),c.getProgram(),c.getDuration());
+   }
 }
--- src/test/java/org/generation/italy/codeSchool/model/data/implementations/CSVFileCourseRepositoryTest.java
+++ src/test/java/org/generation/italy/codeSchool/model/data/implementations/CSVFileCourseRepositoryTest.java
@@ -2,13 +2,16 @@ package org.generation.italy.codeSchool.model.data.implementations;
 
 import org.generation.italy.codeSchool.model.Course;
 import org.generation.italy.codeSchool.model.data.exceptions.DataException;
+import org.generation.italy.codeSchool.model.data.exceptions.EntityNotFoundException;
+import org.junit.jupiter.api.Test;
+
 import java.io.FileOutputStream;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
@@ -17,82 +20,139 @@ import static org.junit.jupiter.api.Assertions.*;       //!!importante!!
 
 class CSVFileCourseRepositoryTest {
 
-    private static final long ID=1;
-    private static final String TITLE="TITLE";
-    private static final String DESCRIPTION="DESCRIPTION";
-    private static final String PROGRAM="PROGRAM";
-    private static final double DURATION=200.0;
-    private static final String CSVLINE=String.format(Locale.US,"%d,%s,%s,%s,%.2f",ID,TITLE,DESCRIPTION,PROGRAM,DURATION);
-    private static final String CSVLINE2=String.format(Locale.US,"%d,%s,%s,%s,%.2f",ID+1,TITLE+"Test",DESCRIPTION+"Test",PROGRAM+"Test",DURATION+1);
-    private static final String FILENAME="TESTDATA.csv";
+   private static final long ID=1;
+   private static final long ID2=ID+1;
+   private static final long ID_NOT_PRESENT = 3;
+   private static final String TITLE="TITLE";
+   private static final String DESCRIPTION="DESCRIPTION";
+   private static final String PROGRAM="PROGRAM";
+   private static final double DURATION=200.0;
+   private static final String CSVLINE=String.format(Locale.US,"%d,%s,%s,%s,%.2f",ID,TITLE,DESCRIPTION,PROGRAM,DURATION);
+   private static final String CSVLINE2=String.format(Locale.US,"%d,%s,%s,%s,%.2f",ID2,TITLE+"Test",DESCRIPTION+"Test",PROGRAM+"Test",DURATION+1);
+   private static final String FILENAME="TESTDATA.csv";
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
-        System.out.println("setUp");
-    }
+   @org.junit.jupiter.api.BeforeEach
+   void setUp() {
+      System.out.println("setUp");
+   }
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-        try {
-            new FileOutputStream(FILENAME).close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+   @org.junit.jupiter.api.AfterEach
+   void tearDown() {
+      try {
+         new FileOutputStream(FILENAME).close();
+      } catch (IOException e) {
+         throw new RuntimeException(e);
+      }
+      System.out.println("tearDown");
+   }
 
-    @org.junit.jupiter.api.Test
-    void findById_finds_course_when_present() {
-        Course c1 = new Course(ID,TITLE,DESCRIPTION,PROGRAM,DURATION);
-        CSVFileCourseRepository  repo = new CSVFileCourseRepository(FILENAME);
-        try(PrintWriter pw = new PrintWriter(new FileOutputStream(FILENAME))){
+   @Test
+   void findById_finds_course_when_present() {
+      Course c1 = new Course(ID,TITLE,DESCRIPTION,PROGRAM,DURATION);
+      CSVFileCourseRepository  repo = new CSVFileCourseRepository(FILENAME);
+      try(PrintWriter pw = new PrintWriter(new FileOutputStream(FILENAME))){
+         pw.println(CSVLINE2);
+         pw.println(CSVLINE);
+         pw.flush();                                         //obbligo a scrivere subito
+         Optional<Course> x = repo.findById(ID);
+         assertTrue(x.isPresent());
+         Course c2 = x.get();
+         assertEquals(c1,c2);
+      }catch (DataException e){
+         fail("Errore nella ricerca by id sul file di testo" + e.getMessage());
+      }catch (IOException e){
+         fail("Errore nella preparazione del test sulla ricerca by id sul file di testo" + e.getMessage());
+      }
+
+      System.out.println("findById");
+   }
+
+   @Test
+   void create() {
+      // ARRANGE
+      Course c = new Course(ID,TITLE,DESCRIPTION,PROGRAM,DURATION);
+      CSVFileCourseRepository  repo = new CSVFileCourseRepository(FILENAME);
+      // ACT
+      try{
+         List<String> linesBefore = Files.readAllLines(Paths.get(FILENAME));
+         repo.create(c);
+         List<String> linesAfter = Files.readAllLines(Paths.get(FILENAME));
+         // ASSERT
+         assertEquals(linesBefore.size()+1,linesAfter.size());
+         String csvLine = linesAfter.get(linesAfter.size()-1);
+         String[] tokens = csvLine.split(",");
+         assertEquals(ID,Long.parseLong(tokens[0]));
+         assertEquals(DURATION,Double.parseDouble(tokens[tokens.length-1]));
+      }catch (DataException e){
+         fail("Fallito il create su file CSV" + e.getMessage());
+      }catch (IOException e){
+         fail("Fallita la verifica del create su file CSV" + e.getMessage());
+      }
+   }
+
+   @Test
+   void courseToCSV() {
+      // ARRANGE      //inizializzo i dati che poi dovrò usare
+      Course c = new Course(ID,TITLE,DESCRIPTION,PROGRAM,DURATION);
+      CSVFileCourseRepository  repo = new CSVFileCourseRepository(FILENAME);
+      // ACT          //richiamo ciò che devo testare
+      String csvLine = repo.CourseToCSV(c);
+      // ASSERT       //prego che tutto sia andato bene
+      //Assertions.assertEquals(1,1);     //possiamo fare assertEquals() perchè l'import è STATIC (quindi evitiamo di scrivere "Assertations." prima)
+      assertEquals(CSVLINE,csvLine);
+   }
+   @Test
+   void deleteById_should_delete_course_when_present() {
+      //Arrange
+      CSVFileCourseRepository repo = new CSVFileCourseRepository(FILENAME);
+      try {
+         try (PrintWriter pw = new PrintWriter(new FileOutputStream(FILENAME))) {
             pw.println(CSVLINE2);
             pw.println(CSVLINE);
-            pw.flush();                                         //obbligo a scrivere subito
-            Optional<Course> x = repo.findById(ID);
-            assertTrue(x.isPresent());
-            Course c2 = x.get();
-            assertEquals(c1,c2);
-        }catch (DataException e){
-            fail("Errore nella ricerca by id sul file di testo" + e.getMessage());
-        }catch (IOException e){
-            fail("Errore nella preparazione del test sulla ricerca by id sul file di testo" + e.getMessage());
-        }
+         }                    //qui creo un file di test
+      //ACT
+         repo.deleteById(ID);
+      //ASSERT
+         List<String[]> tokenLines = readTokenizedLines();
+         assertTrue(tokenLines.size()==1);
+         long courseID = Long.parseLong(tokenLines.get(0)[0]);
+         assertEquals(ID+1,courseID);
+      } catch (IOException e){
+         fail("Errore nell'utilizzo dal file di test CSV");
+      }catch (EntityNotFoundException | DataException e){
+         fail("Errore nella cancellazione di un corso" + e.getMessage());
+      }
+   }
 
-        System.out.println("findById");
-    }
+   @Test
+   void deletById_should_throw_when_course_not_present(){
+      CSVFileCourseRepository repo = new CSVFileCourseRepository(FILENAME);
+      try {
+         try (PrintWriter pw = new PrintWriter(new FileOutputStream(FILENAME))) {
+            pw.println(CSVLINE2);
+            pw.println(CSVLINE);
+         }
+         repo.deleteById(ID_NOT_PRESENT);
+         fail("Non viene lanciata EntityNotFoundException quando si cancella un corso non esistente");
+      }catch(IOException e){
 
-    @org.junit.jupiter.api.Test
-    void create() {
-        // ARRANGE
-        Course c = new Course(ID,TITLE,DESCRIPTION,PROGRAM,DURATION);
-        CSVFileCourseRepository  repo = new CSVFileCourseRepository(FILENAME);
-        // ACT
-        try{
-            List<String> linesBefore = Files.readAllLines(Paths.get(FILENAME));
-            repo.create(c);
-            List<String> linesAfter = Files.readAllLines(Paths.get(FILENAME));
-            // ASSERT
-            assertEquals(linesBefore.size()+1,linesAfter.size());
-            String csvLine = linesAfter.get(linesAfter.size()-1);
-            String[] tokens = csvLine.split(",");
-            assertEquals(ID,Long.parseLong(tokens[0]));
-            assertEquals(DURATION,Double.parseDouble(tokens[tokens.length-1]));
-        }catch (DataException e){
-            fail("Fallito il create su file CSV" + e.getMessage());
-        }catch (IOException e){
-            fail("Fallita la verifica del create su file CSV" + e.getMessage());
-        }
-    }
+      } catch (DataException e) {
+         fail("Errore dell'utilizzo del file CSV");
+      } catch (EntityNotFoundException e) {
+         //Expected
+      }
 
-    @org.junit.jupiter.api.Test
-    void courseToCSV() {
-        // ARRANGE      //inizializzo i dati che poi dovrò usare
-        Course c = new Course(ID,TITLE,DESCRIPTION,PROGRAM,DURATION);
-        CSVFileCourseRepository  repo = new CSVFileCourseRepository(FILENAME);
-        // ACT          //richiamo ciò che devo testare
-        String csvLine = repo.CourseToCSV(c);
-        // ASSERT       //prego che tutto sia andato bene
-        //Assertions.assertEquals(1,1);     //possiamo fare assertEquals() perchè l'import è STATIC (quindi evitiamo di scrivere "Assertations." prima)
-        assertEquals(CSVLINE,csvLine);
-    }
+   }
+
+   private List<String[]> readTokenizedLines() throws IOException{
+      List<String> lines = Files.readAllLines(Paths.get(FILENAME));
+      List<String[]>  tokenLines = new ArrayList<>();
+      for(String s : lines){
+         String[] tokens = s.split(",");
+         tokenLines.add(tokens);
+      }
+      return tokenLines;
+   }
+
+
 }
