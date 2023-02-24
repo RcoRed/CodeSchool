ad8ce475da7d0a6d78c3252ce3f2be6b943cf9ef
jnk626 <jnk626@pm.me>
2023-02-24 12:56:30 +0100

    implements the getModes() method

--- src/main/java/org/generation/italy/codeSchool/model/data/abstractions/CourseEditionRepository.java
+++ src/main/java/org/generation/italy/codeSchool/model/data/abstractions/CourseEditionRepository.java
@@ -1,19 +1,17 @@
 package org.generation.italy.codeSchool.model.data.abstractions;
 
-import org.generation.italy.codeSchool.model.entities.Course;
 import org.generation.italy.codeSchool.model.entities.CourseEdition;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Optional;
 
 public interface CourseEditionRepository {
     double getTotalCost();
-    CourseEdition getMostExpensive();
+    Optional<CourseEdition> getMostExpensive();
     double getAverageCost();
     Iterable<Double> getDurationList();
     Iterable<CourseEdition> getCourseEditionsById(long courseId);
     Iterable<CourseEdition> getCourseEditionsInTime(String name, LocalDate from, LocalDate to);
     Iterable<CourseEdition> getMiddleCourseEdition();
-    Course getMode();
-
+    Optional<Double> getModes();
 }
--- src/main/java/org/generation/italy/codeSchool/model/data/implementations/InMemoryCourseEditionRepository.java
+++ src/main/java/org/generation/italy/codeSchool/model/data/implementations/InMemoryCourseEditionRepository.java
@@ -6,6 +6,9 @@ import org.generation.italy.codeSchool.model.entities.CourseEdition;
 
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.counting;
 
 public class InMemoryCourseEditionRepository implements CourseEditionRepository {
 
@@ -17,8 +20,9 @@ public class InMemoryCourseEditionRepository implements CourseEditionRepository 
     }
 
     @Override
-    public CourseEdition getMostExpensive() {
-        return editionCollection.stream().sorted((ce1, ce2) -> (int) (ce1.getCost() - ce2.getCost())).toList().get(0);
+    public Optional<CourseEdition> getMostExpensive() {
+        //return editionCollection.stream().sorted((ce1, ce2) -> (int) (ce1.getCost() - ce2.getCost())).toList().get(0);
+        return editionCollection.stream().max(Comparator.comparingDouble(CourseEdition::getCost));
     }
 
     @Override
@@ -40,28 +44,33 @@ public class InMemoryCourseEditionRepository implements CourseEditionRepository 
     public List<CourseEdition> getCourseEditionsInTime(String name, LocalDate fromDate, LocalDate toDate) {
         return editionCollection.stream().filter(e -> e.getCourse().getTitle().contains(name)
                     && e.getStartedAt().isAfter(fromDate)
-                    && e.getStartedAt().isBefore(toDate)).
-                    toList();
+                    && e.getStartedAt().isBefore(toDate))
+                    .toList();
     }
 
     @Override
     public List<CourseEdition> getMiddleCourseEdition() {
         List<CourseEdition> result = new ArrayList<>();
         List<CourseEdition> sorted = editionCollection.stream()
-                .sorted(Comparator.comparingDouble(CourseEdition::getCost)).
-                toList();
+                                                      .sorted(Comparator.comparingDouble(CourseEdition::getCost))
+                                                      .toList();
         if (editionCollection.size() % 2 == 0) {
             result.add(sorted.get((sorted.size() / 2)));
             result.add(sorted.get((sorted.size() / 2) + 1));
         } else {
-            result.add(sorted.get((sorted.size() + 1) / 2));
+            result.add(sorted.get((sorted.size() - 1) / 2));
         }
         return result;
     }
 
     @Override
-    public Course getMode() {
-        return null;
-
+    public Optional<Double> getModes() {
+                return editionCollection.stream()
+                                 .collect(Collectors.groupingBy(CourseEdition::getCost, Collectors.counting()))
+                                 .entrySet()
+                                 .stream()
+                                 .max(Comparator.comparingLong(Map.Entry::getValue))
+                                 .map(Map.Entry::getKey); // map funziona su un Optional! Tira fuori il valore dell'Optional
+                                                          // e lo trasforma nella sua stessa chiave sempre come Optional!
     }
 }
