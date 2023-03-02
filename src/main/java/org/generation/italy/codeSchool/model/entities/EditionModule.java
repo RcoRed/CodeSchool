7b796dba3b46fffc14636997273354d7aa0d221e
RcoRed <notpurple.red@gmail.com>
2023-03-02 20:44:09 +0100

    Aggiunge a EdiotionModule la variabile di tipo EditionCourse

--- src/main/java/org/generation/italy/codeSchool/model/entities/EditionModule.java
+++ src/main/java/org/generation/italy/codeSchool/model/entities/EditionModule.java
@@ -5,7 +5,8 @@ import java.time.LocalDate;
 public class EditionModule {
     private long id;
     private CourseModule courseModule;
-    private EditionModule editionModule;
+    private CourseEdition courseEdition;
+    //private EditionModule editionModule;  ???
     private Teacher teacher;
     private LocalDate startDate;
     private LocalDate endDate;
