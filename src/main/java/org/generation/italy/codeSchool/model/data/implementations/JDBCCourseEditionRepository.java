66ccf3609e70336ce09b8b461e3e2cec280eb136
DavideFailla <davide.failla4@gmail.com>
2023-03-09 11:54:58 +0100

    Aggiunge test JDBCCourseEditionRepositoryTest (metodo findMostExpensive) e metodo update generale in JDBCTestUtils

--- src/main/java/org/generation/italy/codeSchool/model/data/implementations/JDBCCourseEditionRepository.java
+++ src/main/java/org/generation/italy/codeSchool/model/data/implementations/JDBCCourseEditionRepository.java
@@ -1,11 +1,17 @@
 package org.generation.italy.codeSchool.model.data.implementations;
 
 import org.generation.italy.codeSchool.model.data.abstractions.CourseEditionRepository;
+import org.generation.italy.codeSchool.model.entities.Classroom;
+import org.generation.italy.codeSchool.model.entities.Course;
 import org.generation.italy.codeSchool.model.entities.CourseEdition;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import static org.generation.italy.codeSchool.model.data.JDBCConstants.*;
 
 public class JDBCCourseEditionRepository implements CourseEditionRepository {
 
@@ -23,7 +29,17 @@ public class JDBCCourseEditionRepository implements CourseEditionRepository {
 
     @Override
     public Optional<CourseEdition> findMostExpensive() {
-        return Optional.empty();
+        try(
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery(MOST_EXPENSIVE_COURSE_EDITION)
+                ){
+            if(rs.next()){
+                return Optional.of(databaseToCourseEdition(rs));
+            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -43,7 +59,22 @@ public class JDBCCourseEditionRepository implements CourseEditionRepository {
 
     @Override
     public Iterable<CourseEdition> findByCourseTitleAndPeriod(long courseId, String titlePart, LocalDate startAt, LocalDate endAt) {
-        return null;
+        try (
+                PreparedStatement st = con.prepareStatement(FIND_BY_COURSE_TITLE_AND_PERIOD)
+                ){
+            st.setString(1,"%" + titlePart + "%");
+            st.setDate(2, Date.valueOf(startAt));
+            st.setDate(3, Date.valueOf(endAt));
+            try (ResultSet rs = st.executeQuery()){
+                List<CourseEdition> ce = new ArrayList<>();
+                while (rs.next()){
+                    ce.add(databaseToCourseEdition(rs));
+                }
+                return ce;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -56,7 +87,15 @@ public class JDBCCourseEditionRepository implements CourseEditionRepository {
         return Optional.empty();
     }
 
-    public Iterable<CourseEdition> findByTeacherId(long id){
+    public Optional<CourseEdition> findByTeacherId(long id){
         return null;
+    }
+
+    private CourseEdition databaseToCourseEdition(ResultSet rs) throws SQLException{
+        Classroom cr = new Classroom(rs.getLong(11), rs.getString(12),rs.getInt(13),
+                rs.getBoolean(14), rs.getBoolean(15), rs.getBoolean(16),null);
+        Course c = new Course(rs.getInt(4), rs.getString(5),rs.getString(6),rs.getString(7),
+                rs.getDouble(8), rs.getBoolean(9), rs.getDate(10).toLocalDate());
+        return new CourseEdition(rs.getLong(1),c,rs.getDate(2).toLocalDate(),rs.getDouble(3),cr);
     }
 }
