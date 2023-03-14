a15c354c2fcb7b7d86de50124031f7488cd156fc
danielePalazzo <palazzo703@gmail.com>
2023-03-14 11:18:27 +0100

    bug fix

--- src/main/java/org/generation/italy/codeSchool/model/data/implementations/GenericCrudRepository.java
+++ src/main/java/org/generation/italy/codeSchool/model/data/implementations/GenericCrudRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-public class GenericCrudRepository<T> implements AbstractCrudRepository {
+public class GenericCrudRepository<T> implements AbstractCrudRepository<T> {
     @Autowired
     protected Session session;
     protected Class<?> entityClass;
