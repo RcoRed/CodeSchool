700580f08c47efaad9d5f118b1d9eaf5cdaf6952
DavideDeLuca019 <davide.de.luca2002@gmail.com>
2023-03-14 11:13:07 +0100

    Aggiunge implementazione per Repository Crud Hibernate generico

--- src/main/java/org/generation/italy/codeSchool/model/data/implementations/HibernateCourseRepository.java
+++ src/main/java/org/generation/italy/codeSchool/model/data/implementations/HibernateCourseRepository.java
@@ -1,59 +1,61 @@
 package org.generation.italy.codeSchool.model.data.implementations;
 
-import jakarta.persistence.Query;
 import org.generation.italy.codeSchool.model.data.abstractions.CourseRepository;
 import org.generation.italy.codeSchool.model.data.exceptions.DataException;
 import org.generation.italy.codeSchool.model.data.exceptions.EntityNotFoundException;
 import org.generation.italy.codeSchool.model.entities.Course;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 @Repository
-public class HibernateCourseRepository implements CourseRepository {
-    @Autowired
-    private Session session;
-    @Override
-    public List<Course> findAll() throws DataException {
-        org.hibernate.query.Query<Course> q = session.createQuery("from Course", Course.class); //utilizza query di Hibernate
-        return q.list();
+public class HibernateCourseRepository extends GenericCrudRepository<Course> implements CourseRepository {
+//    @Autowired
+//    private Session session;
+//    @Override
+//    public List<Course> findAll() throws DataException {
+//        Query<Course> q = session.createQuery("from Course", Course.class); //utilizza query di Hibernate
+//        return q.list();
+//    }
+//
+//    @Override
+//    public Optional<Course> findById(long id) throws DataException {
+//        Course found = session.get(Course.class,id);
+//        return found!=null ? Optional.of(found) : Optional.empty();
+//    }
+//
+//    @Override
+//    public Course create(Course course) throws DataException {
+//        session.persist(course);
+//        return course;
+//    }
+//
+//    @Override
+//    public void update(Course course) throws EntityNotFoundException, DataException {
+//        session.merge(course);
+//    }
+//
+//    @Override
+//    public void deleteById(long id) throws EntityNotFoundException, DataException {
+//        Course c = session.getReference(Course.class,id);
+//        session.remove(c);
+//    }
+//
+//
+//
+//
+//
+    public HibernateCourseRepository(Session s){
+        super(s, Course.class);
     }
-
-    @Override
-    public Optional<Course> findById(long id) throws DataException {
-        Course found = session.get(Course.class,id);
-        return found!=null ? Optional.of(found) : Optional.empty();
-    }
-
-    @Override
-    public List<Course> findByTitleContains(String part) throws DataException {
-        Query q = session.createQuery("from Course where title like :p", Course.class);
-        q.setParameter("p",part);
-        return q.getResultList();
-    }
-
-    @Override
-    public Course create(Course course) throws DataException {
-        session.persist(course);
-        return course;
-    }
-
-    @Override
-    public void update(Course course) throws EntityNotFoundException, DataException {
-        session.merge(course);
-    }
-
-    @Override
-    public void deleteById(long id) throws EntityNotFoundException, DataException {
-        Course c = session.getReference(Course.class,id);
-        session.remove(c);
-    }
-
     @Override
     public int countActiveCourses() throws DataException {
-        return 0;
+        Query<Integer> q = session.createQuery("select count (*) from Course where isActive = true ", Integer.class);
+        int n = q.getSingleResult();
+        return n;
     }
 
     @Override
@@ -64,5 +66,11 @@ public class HibernateCourseRepository implements CourseRepository {
     @Override
     public boolean adjustActiveCourses(int NumActive) throws DataException {
         return false;
+    }
+    @Override
+    public List<Course> findByTitleContains(String part) throws DataException {
+        Query<Course> q = session.createQuery("from Course where title like :p", Course.class);
+        q.setParameter("p",part);
+        return q.list();
     }
 }
