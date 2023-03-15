e6fc528c7e141e076be160d6b83cc3741b0acf0a
vittorioavataneo <vittorioavataneo@gmail.com>
2023-03-15 12:12:50 +0100

    Corregge l'ordinamento per la query per l'ordinamento dei corsi vecchi

--- src/main/java/org/generation/italy/codeSchool/model/data/HibernateConstants.java
+++ src/main/java/org/generation/italy/codeSchool/model/data/HibernateConstants.java
@@ -5,7 +5,7 @@ public class HibernateConstants {
     public static final String HQL_DEACTIVATE_OLDEST_N_COURSES = """
                update Course c set c.isActive=false where c in (
                select co from Course co where co.isActive = true
-               order by co.id
+               order by co.createdAt
                limit :limit
             )
             """;
