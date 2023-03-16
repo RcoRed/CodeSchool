2bfa78dd924b68206a83a6eeffd5345d837aa390
GabrielAgustinIppolito <gabrielpplt@gmail.com>
2023-03-16 11:33:56 +0100

    Corregge il Case nell'interfaccia per il repository CRUD

--- src/main/java/org/generation/italy/codeSchool/model/data/abstractions/AbstractCrudRepository.java
+++ src/main/java/org/generation/italy/codeSchool/model/data/abstractions/AbstractCrudRepository.java
@@ -5,11 +5,12 @@ import org.generation.italy.codeSchool.model.data.exceptions.EntityNotFoundExcep
 
 import java.util.List;
 import java.util.Optional;
+//DAO == Repository... Data Access Object
 
-public interface AbstractCrudRepository <T> {
-    List<T> findAll() throws DataException;
-    Optional<T> findById(long id) throws DataException;
-    T create(T entity) throws DataException;
-    void update(T entity) throws EntityNotFoundException, DataException;
-    void deleteById(long id) throws EntityNotFoundException, DataException;
+public interface AbstractCrudRepository<T>{
+   List<T> findAll() throws DataException;
+   Optional<T> findById(long id) throws DataException;
+   T create(T entity) throws DataException;
+   void update(T entity) throws EntityNotFoundException, DataException;
+   void deleteById(long id) throws EntityNotFoundException, DataException;
 }
